aad4a0859a173cac5d198c947b7f742824653d1c
hibbahagh <hibbahagh@gmail.com>
2018-06-06 21:51:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,25 @@
   # if number is divisible by 5 then it's buzz 
   # if number is divisible by 5 & 3 then it's fizzbuzz
   
+# def fizzbuzz(int)
+#   if int % 3 == 0 && int % 5 == 0 
+#     puts "fizzbuzz"
+#     elsif int % 3 == 0 
+#     puts "fizz"
+#     elsif int % 5 == 0 
+#     puts "buzz"
+#   else 
+#     puts "nil"
+#   end 
+# end 
+    
+# fizzbuzz(2)
+
+
+puts "what's your number"
+int = gets.chomp.to_i
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
+  if int % 15 == 0 
     puts "fizzbuzz"
     elsif int % 3 == 0 
     puts "fizz"
@@ -14,5 +31,4 @@ def fizzbuzz(int)
     puts "nil"
   end 
 end 
-    
-fizzbuzz(2)
+fizzbuzz(int)
